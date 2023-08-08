c445355a2054ae4837e5427185bdfaf97e8b62ff
Aluno <kaua.moreno2005@gmail.com>
2023-08-08 10:50:31 -0300

    :lipstick: feat: Constantes para estilização de interface

---
+++ lib/constants/Cores.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Cores{
+
+  //Vermelho
+  static const Color red = Color(0xdc3545);
+  static const Color redDanger = Color(0xE30613);
+  
+  //Claro
+  static const Color white = Color(0xffffff);
+  static const Color light = Color(0xf8f9fa);
+
+  //Escuro
+  static const Color gray = Color(0x6c757d);
+  static const Color black = Color(0x333333);
+
+}
---
+++ lib/constants/FonteSize.dart
@@ -0,0 +1,7 @@
+class FontSize {
+
+  static const double small = 12.0;
+  static const double medium = 16.0;
+  static const double large = 20.0;
+
+}
