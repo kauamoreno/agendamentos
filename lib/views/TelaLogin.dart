f16e10dacbc26152dbd8be39b7436e2377bbb16b
Aluno <nikolaslifesenai@gmail.com>
2023-08-08 11:13:35 -0300

    :sparkles: feat: Criando página de login

---
+++ lib/views/TelaLogin.dart
@@ -0,0 +1,105 @@
+import 'package:flutter/material.dart';
+import 'package:agendamentos/constants/Cores.dart';
+
+class TelaLogin extends StatefulWidget {
+  @override
+  _TelaLoginState createState() => _TelaLoginState();
+}
+
+class _TelaLoginState extends State<TelaLogin> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Container(
+        width: MediaQuery.sizeOf(context).width,
+        height: MediaQuery.sizeOf(context).height,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              width: MediaQuery.sizeOf(context).width,
+              height: MediaQuery.sizeOf(context).height * 0.4,
+              alignment: AlignmentDirectional(0, 0),
+              child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'AGS',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                          fontSize: 100,
+                          fontWeight: FontWeight.w900,
+                          fontStyle: FontStyle.italic),
+                    ),
+                  ]),
+            ),
+            Container(
+              width: MediaQuery.sizeOf(context).width,
+              height: MediaQuery.sizeOf(context).height * 0.45,
+              decoration: BoxDecoration(
+                color: Color(0xFFB71835),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(0),
+                  bottomRight: Radius.circular(0),
+                  topLeft: Radius.circular(20),
+                  topRight: Radius.circular(20),
+                ),
+                shape: BoxShape.rectangle,
+              ),
+              child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+                  child: Form(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        TextField(
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              labelText: "Email",
+                              hintText: "email@mail.com",
+                              prefixIcon: Padding(
+                                child: Icon(Icons.email),
+                                padding: EdgeInsets.all(5),
+                              )),
+                        ),
+                        TextField(
+                          keyboardType: TextInputType.visiblePassword,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              labelText: "Senha",
+                              prefixIcon: Padding(
+                                child: Icon(Icons.password),
+                                padding: EdgeInsets.all(5),
+                              )),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          child: Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Text(
+                              'Entrar',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  )),
+            )
+          ],
+        ),
+      ),
+    ));
+  }
+}
