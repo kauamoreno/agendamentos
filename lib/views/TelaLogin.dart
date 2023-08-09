50a0c8d26bef742f975a50bc6110488a68d27a16
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-08-09 18:08:44 -0300

    :ok_hand: style: Corrigindo indentação inconsistente no arquivo TelaLogin.dart

--- lib/views/TelaLogin.dart
+++ lib/views/TelaLogin.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:agendamentos/constants/Cores.dart';
+// import 'package:agendamentos/constants/Cores.dart';
 
 class TelaLogin extends StatefulWidget {
   @override
@@ -9,26 +9,28 @@ class TelaLogin extends StatefulWidget {
 class _TelaLoginState extends State<TelaLogin> {
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: Container(
-        width: MediaQuery.sizeOf(context).width,
-        height: MediaQuery.sizeOf(context).height,
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              width: MediaQuery.sizeOf(context).width,
-              height: MediaQuery.sizeOf(context).height * 0.4,
-              alignment: AlignmentDirectional(0, 0),
-              child: Column(
+      body: SingleChildScrollView(
+
+        child: Container(
+          width: MediaQuery.sizeOf(context).width,
+          height: MediaQuery.sizeOf(context).height,
+
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+            children: [
+              Container(
+                width: MediaQuery.sizeOf(context).width,
+                height: MediaQuery.sizeOf(context).height * 0.4,
+                alignment: AlignmentDirectional(0, 0),
+                child: Column(
                   mainAxisSize: MainAxisSize.max,
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
@@ -36,26 +38,28 @@ class _TelaLoginState extends State<TelaLogin> {
                       'AGS',
                       textAlign: TextAlign.center,
                       style: TextStyle(
-                          fontSize: 100,
-                          fontWeight: FontWeight.w900,
-                          fontStyle: FontStyle.italic),
+                        fontSize: 100,
+                        fontWeight: FontWeight.w900,
+                        fontStyle: FontStyle.italic
+                      ),
                     ),
-                  ]),
-            ),
-            Container(
-              width: MediaQuery.sizeOf(context).width,
-              height: MediaQuery.sizeOf(context).height * 0.45,
-              decoration: BoxDecoration(
-                color: Color(0xFFB71835),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(0),
-                  bottomRight: Radius.circular(0),
-                  topLeft: Radius.circular(20),
-                  topRight: Radius.circular(20),
+                  ]
                 ),
-                shape: BoxShape.rectangle,
               ),
-              child: Padding(
+              Container(
+                width: MediaQuery.sizeOf(context).width,
+                height: MediaQuery.sizeOf(context).height * 0.45,
+                decoration: BoxDecoration(
+                  color: Color(0xFFB71835),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(0),
+                    bottomRight: Radius.circular(0),
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                  shape: BoxShape.rectangle,
+                ),
+                child: Padding(
                   padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
                   child: Form(
                     child: Column(
@@ -66,23 +70,27 @@ class _TelaLoginState extends State<TelaLogin> {
                         TextField(
                           keyboardType: TextInputType.emailAddress,
                           decoration: InputDecoration(
-                              labelText: "Email",
-                              hintText: "email@mail.com",
-                              prefixIcon: Padding(
-                                child: Icon(Icons.email),
-                                padding: EdgeInsets.all(5),
-                              )),
+                            labelText: "Email",
+                            hintText: "email@mail.com",
+                            prefixIcon: Padding(
+                              child: Icon(Icons.email),
+                              padding: EdgeInsets.all(5),
+                            )
+                          ),
                         ),
+
                         TextField(
                           keyboardType: TextInputType.visiblePassword,
                           obscureText: true,
                           decoration: InputDecoration(
-                              labelText: "Senha",
-                              prefixIcon: Padding(
-                                child: Icon(Icons.password),
-                                padding: EdgeInsets.all(5),
-                              )),
+                            labelText: "Senha",
+                            prefixIcon: Padding(
+                              child: Icon(Icons.password),
+                              padding: EdgeInsets.all(5),
+                            )
+                          ),
                         ),
+                          
                         ElevatedButton(
                           onPressed: () {},
                           child: Padding(
@@ -95,11 +103,13 @@ class _TelaLoginState extends State<TelaLogin> {
                         )
                       ],
                     ),
-                  )),
-            )
-          ],
+                  )
+                ),
+              )
+            ],
+          ),
         ),
-      ),
-    ));
+      )
+    );
   }
 }
