606d8555057bb7a51f14a170f0abd079c5365ae9
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-08-13 20:03:39 -0300

    :sparkles: feat: Criando uma SnackBar customizável

---
+++ lib/components/MensagemSnackBar.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import '../constants/Cores.dart';
+
+class MensagemSnackBar {
+
+  static void _show(BuildContext context, String mensagem, IconData icone, Color cor) {
+    final snackBar = SnackBar(
+      content: Row(
+        children: [
+          Icon(
+            icone,
+            color: Cores.white
+          ),
+          SizedBox(width: 10),
+          Text( 
+            mensagem, 
+            style: TextStyle(
+              color: Cores.white
+            )
+          )
+        ],
+      ),
+      backgroundColor: cor,
+      behavior: SnackBarBehavior.floating,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+    );
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
+  showErro(BuildContext context, String erroMensagem){
+    _show(context, erroMensagem, Icons.error_outline, Cores.redDanger);
+  }
+
+  showSucesso(BuildContext context, String erroMensagem){
+    _show(context, erroMensagem, Icons.check_circle_outline, Cores.azul);
+  }
+  
+}
