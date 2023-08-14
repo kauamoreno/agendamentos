e4a1ea4ddfe496c734bc1414d02a3b73d2690778
Kevin Taiyo <119669310+kevintaiyo@users.noreply.github.com>
2023-08-14 07:59:47 -0300

    :construction: feat: Implementando funcionamento da navbar

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -1,39 +1,54 @@
 import 'dart:html';
 import 'package:flutter/material.dart';
+import '../App.dart';
+import 'Perfil.dart';
 
 class Home extends StatefulWidget {
-  const Home({ Key? key }) : super(key: key);
+  const Home({Key? key}) : super(key: key);
 
   @override
   State<Home> createState() => _HomeState();
 }
 
-
 class _HomeState extends State<Home> {
+  int pageSelecionada = 0;
+
+  void itemSelecionado(int index) {
+    setState(() {
+      pageSelecionada = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-       appBar: AppBar(
+      appBar: AppBar(
         title: Text("Home"),
       ),
-      
-       //NAVIGATION BAR
+
+      //NAVIGATION BAR
       body: Column(
-        children: <Widget> [],
+        children: <Widget>[],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.red,
         items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: "Home"
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person_3_sharp),
-            label: "Perfil"
-          )
+              icon: Icon(Icons.person_3_sharp), label: "Perfil")
         ],
-        selectedItemColor: Colors.white
+        selectedItemColor: Colors.white,
+        currentIndex: pageSelecionada,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.pushNamed(context, '/home');
+              break;
+            case 1:
+              Navigator.pushNamed(context, '/perfil');
+              break;
+          }
+        },
       ),
     );
   }
--- lib/views/Perfil.dart
+++ lib/views/Perfil.dart
@@ -1,23 +1,33 @@
 import 'package:flutter/material.dart';
+import 'Home.dart';
+import '../App.dart';
 
 class Perfil extends StatefulWidget {
-  const Perfil({ Key? key }) : super(key: key);
+  const Perfil({Key? key}) : super(key: key);
 
   @override
   State<Perfil> createState() => _PerfilState();
 }
 
 class _PerfilState extends State<Perfil> {
+  int pageSelecionada = 0;
+  late PageController animationPage;
+
+  void itemSelecionado(int index) {
+    setState(() {
+      pageSelecionada = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Perfil do Usuario"),
       ),
-
       //NAVIGATION BAR
       body: Column(
-        children: <Widget> [],
+        children: <Widget>[],
       ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.red,
@@ -25,14 +35,22 @@ class _PerfilState extends State<Perfil> {
           BottomNavigationBarItem(
             icon: Icon(Icons.home),
             label: "Home",
-            
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.person_3_outlined),
-            label: "Perfil"
-          )
+              icon: Icon(Icons.person_3_outlined), label: "Perfil")
         ],
-        selectedItemColor: Colors.white
+        selectedItemColor: Colors.white,
+        currentIndex: pageSelecionada,
+        onTap: (index) {
+          switch (index) {
+            case 0:
+              Navigator.pushNamed(context, '/home');
+              break;
+            case 1:
+              Navigator.pushNamed(context, '/perfil');
+              break;
+          }
+        },
       ),
     );
   }
