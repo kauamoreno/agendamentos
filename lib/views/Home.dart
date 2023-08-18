7097723af8c295180e290c021844d4348ac3c522
Aluno <kevintaiyo17@gmail.com>
2023-08-18 11:26:27 -0300

    :sparkles: feat: Troca entre telas pela BottomNavigationBar

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -2,6 +2,7 @@ import 'dart:html';
 import 'package:flutter/material.dart';
 import '../App.dart';
 import 'Perfil.dart';
+import '../components/MensagemSnackBar.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -17,6 +18,14 @@ class _HomeState extends State<Home> {
     setState(() {
       pageSelecionada = index;
     });
+    switch (index) {
+                case 0:
+                  Navigator.pushReplacementNamed(context, '/home');
+                  break;
+                case 1:
+                  Navigator.pushReplacementNamed(context, '/perfil');
+                  break;
+          }
   }
 
   @override
@@ -24,31 +33,20 @@ class _HomeState extends State<Home> {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
+        automaticallyImplyLeading: false, //USADO PARA REMOVER A SETINHA DO APPBAR
       ),
 
       //NAVIGATION BAR
-      body: Column(
-        children: <Widget>[],
-      ),
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.red,
+        currentIndex: pageSelecionada,
+        onTap:itemSelecionado,
         items: [
           BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
           BottomNavigationBarItem(
               icon: Icon(Icons.person_3_sharp), label: "Perfil")
         ],
         selectedItemColor: Colors.white,
-        currentIndex: pageSelecionada,
-        onTap: (index) {
-          switch (index) {
-            case 0:
-              Navigator.pushNamed(context, '/home');
-              break;
-            case 1:
-              Navigator.pushNamed(context, '/perfil');
-              break;
-          }
-        },
       ),
     );
   }
