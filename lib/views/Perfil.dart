08541503f02dd1b63bcb306af90fa2b898d42b16
unknown <kevintaiyo17@gmail.com>
2023-08-20 16:01:47 -0300

    :lipstick: feat: estilizando a pagina perfil para futuro funcionamento

--- lib/views/Perfil.dart
+++ lib/views/Perfil.dart
@@ -27,16 +27,123 @@ class _PerfilState extends State<Perfil> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         title: Text("Perfil do Usuario"),
-        automaticallyImplyLeading: false, //USADO PARA REMOVER A SETINHA DO APPBAR
+        automaticallyImplyLeading:
+            false, //USADO PARA REMOVER A SETINHA DO APPBAR
       ),
-
-      body: Column(
-        children: <Widget>[],
+      body: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  width: 120,
+                  height: 120,
+                  clipBehavior: Clip.antiAlias,
+                  decoration:
+                      BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                  child: Image.network(
+                    'https://www.offidocs.com/images/xtwitterdefaultpfpicon.jpg.pagespeed.ic.9q2wXBQmsW.jpg',
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                Text('Carlinhos\nInstrutor\nSenai - Nami Jafet 117',
+                    style: TextStyle(
+                        fontSize: 20,
+                        fontFamily: 'Readex Pro',
+                        color: Colors.black)),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+              child: Card(
+                clipBehavior: Clip.antiAliasWithSaveLayer,
+                color: Color(0xFFF7F2F9),
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(
+                      Icons.schedule_rounded,
+                      color: Color(0xFF650000),
+                      size: 64,
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                      child: Text(
+                        'Minhas Agendas',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontFamily: 'Readex Pro',
+                          color: Color(0xFF650000),
+                          fontSize: 28,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Expanded(
+                child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                    child: Card(
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      color: Color(0xFFF7F2F9),
+                      elevation: 2,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(
+                            Icons.flight_class,
+                            color: Color(0xFF650000),
+                            size: 64,
+                          ),
+                          Padding(
+                            padding:
+                                EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                            child: Text(
+                              'Sala 7:\nSegunda-Feira\n7h30-11h30',
+                              textAlign: TextAlign.start,
+                              style: TextStyle(
+                                fontFamily: 'Readex Pro',
+                                color: Color(0xFF650000),
+                                fontSize: 28,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ))
+          ],
+        ),
       ),
-
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: Colors.red,
         currentIndex: pageSelecionada,
@@ -46,7 +153,8 @@ class _PerfilState extends State<Perfil> {
             icon: Icon(Icons.home),
             label: "Home",
           ),
-          BottomNavigationBarItem(icon: Icon(Icons.person_3_sharp), label: "Perfil")
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_3_sharp), label: "Perfil")
         ],
         selectedItemColor: Colors.white,
       ),
