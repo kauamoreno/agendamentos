4e0f2d0c197ebb88cf39eaa2c82e4aae3372cb7a
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-08-20 08:38:11 -0300

    :sparkles: feat: Adicionando métodos firestore para a coleção que contem salas

---
+++ lib/services/Firestore/SalasFirestore.dart
@@ -0,0 +1,66 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import '../../components/MensagemSnackBar.dart';
+
+class SalasFirestore {
+
+  MensagemSnackBar mensagemSnackBar = MensagemSnackBar();
+  CollectionReference<Map<String, dynamic>> db = FirebaseFirestore.instance.collection('salaConjunto');
+
+  //CREATE
+  criarSalaConjunto(String nome) {
+    final salaConjunto = {
+      "nome": "Mecânica",
+      "salas": [
+        {"nome": "Sala1", "capacidade": 32, "isLivre": true},
+        {"nome": "Sala2", "capacidade": 16, "isLivre": false},
+      ]
+    };
+
+    db.doc().set(salaConjunto);
+  }
+
+  //DELETE
+  deletarSala(BuildContext context, String idConjunto, String idSala) {
+    DocumentReference salaConjuntoDocRef = db.doc(idConjunto);
+    CollectionReference salasCollection = salaConjuntoDocRef.collection('salas');
+    
+    salasCollection.doc(idSala).delete().then(
+      (doc) => mensagemSnackBar.showSucesso(context, "Documento deletado"),
+      onError: (e) => mensagemSnackBar.showErro(context, "Erro em apagar o usuário"),
+    );
+  }
+
+  //READ SALACONJUNTO
+  Future<Map<String, dynamic>?> getSalaConjunto(String idConjunto) async {
+    final docRef = db.doc(idConjunto);
+
+    try {
+      DocumentSnapshot doc = await docRef.get();
+      Map<String, dynamic> salaConjunto = doc.data() as Map<String, dynamic>;
+      return salaConjunto;
+
+    } catch (e) {
+      print("Erro: $e");
+      return null;
+    }
+  }
+
+  //READ SALA ISLIVRE
+  Future<bool?> getSalaIsLivre(String idConjunto, String idSala) async {
+
+    Map<String, dynamic>? salaConjuntoMap = await getSalaConjunto(idConjunto);
+
+    if (salaConjuntoMap != null && salaConjuntoMap.containsKey('salas')) {
+      List<dynamic> salas = salaConjuntoMap['salas'];
+
+      for (dynamic sala in salas) {
+        if (sala['id'] == idSala) {
+          return sala['livre'] as bool;
+        }
+      }
+    }
+
+    return null;
+  }
+}
