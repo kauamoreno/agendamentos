f799a9582920bdfe8edbe19a4803df2cf1efa6cc
Kevin Taiyo <119669310+kevintaiyo@users.noreply.github.com>
2023-08-22 08:12:42 -0300

    :lipstick: feat: Estilizando a página perfil e arrumando scroll do BottomNavigationBar
    * :lipstick: feat: estilizando a pagina perfil para futuro funcionamento
    
    * :goal_net: tratar erro na bottom navigation bar

--- lib/views/Home.dart
+++ lib/views/Home.dart
@@ -1,46 +1,50 @@
+import 'package:agendamentos/constants/Cores.dart';
 import 'package:flutter/material.dart';
+import 'Perfil.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
-
   @override
   State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  int pageSelecionada = 0;
+  @override
+  int paginaAtual = 0;
+  PageController pc = PageController();
 
-  void itemSelecionado(int index) {
+  void initState() {
+    super.initState();
+    pc = PageController(initialPage: paginaAtual);
+  }
+
+  setPagina(pagina) {
     setState(() {
-      pageSelecionada = index;
+      paginaAtual = pagina;
     });
-    switch (index) {
-      case 0:
-        Navigator.pushReplacementNamed(context, '/home');
-        break;
-      case 1:
-        Navigator.pushReplacementNamed(context, '/perfil');
-        break;
-    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Home"),
-        automaticallyImplyLeading: false, //USADO PARA REMOVER A SETINHA DO APPBAR
+      body: PageView(
+        controller: pc,
+        children: [Home(), Perfil()],
+        onPageChanged: setPagina,
       ),
-
       bottomNavigationBar: BottomNavigationBar(
+        currentIndex: paginaAtual,
         backgroundColor: Colors.red,
-        currentIndex: pageSelecionada,
-        onTap: itemSelecionado,
         items: [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: "Home"),
-          BottomNavigationBarItem(icon: Icon(Icons.person_3_sharp), label: "Perfil")
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.person_2_sharp), label: 'Perfil'),
         ],
-        selectedItemColor: Colors.white,
+        selectedItemColor: Cores.white,
+        onTap: (pagina) {
+          pc.animateToPage(pagina,
+              duration: Duration(milliseconds: 400), curve: Curves.ease);
+        },
       ),
     );
   }
