99ec5cd2ba5eeec066a393643b6ebe8f5ebcafa9
Aluno <kaua.moreno2005@gmail.com>
2023-08-30 10:23:58 -0300

    :ok_hand: style: Corrigindo indentação inconsistente de Perfil.dart

--- lib/views/pages/Perfil.dart
+++ lib/views/pages/Perfil.dart
@@ -28,20 +28,22 @@ class _PerfilState extends State<Perfil> {
                   width: 120,
                   height: 120,
                   clipBehavior: Clip.antiAlias,
-                  decoration:
-                      BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                  decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red),
                   child: Image.network(
                     'https://www.offidocs.com/images/xtwitterdefaultpfpicon.jpg.pagespeed.ic.9q2wXBQmsW.jpg',
                     fit: BoxFit.cover,
                   ),
                 ),
                 Text('Carlinhos\nInstrutor\nSenai - Nami Jafet 117',
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontFamily: 'Readex Pro',
-                        color: Colors.black)),
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontFamily: 'Readex Pro',
+                    color: Colors.black
+                  )
+                ),
               ],
             ),
+
             Padding(
               padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
               child: Card(
@@ -78,51 +80,52 @@ class _PerfilState extends State<Perfil> {
               ),
             ),
             Expanded(
-                child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
-                    child: Card(
-                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                      color: Color(0xFFF7F2F9),
-                      elevation: 2,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Icon(
-                            Icons.flight_class,
-                            color: Color(0xFF650000),
-                            size: 64,
-                          ),
-                          Padding(
-                            padding:
-                                EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
-                            child: Text(
-                              'Sala 7:\nSegunda-Feira\n7h30-11h30',
-                              textAlign: TextAlign.start,
-                              style: TextStyle(
-                                fontFamily: 'Readex Pro',
-                                color: Color(0xFF650000),
-                                fontSize: 28,
-                                fontWeight: FontWeight.w500,
-                              ),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 5, 0, 5),
+                      child: Card(
+                        clipBehavior: Clip.antiAliasWithSaveLayer,
+                        color: Color(0xFFF7F2F9),
+                        elevation: 2,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8),
+                        ),
+                        child: Row(
+                          mainAxisSize: MainAxisSize.max,
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Icon(
+                              Icons.flight_class,
+                              color: Color(0xFF650000),
+                              size: 64,
                             ),
-                          )
-                        ],
+                            Padding(
+                              padding: EdgeInsetsDirectional.fromSTEB(0, 10, 0, 10),
+                              child: Text(
+                                'Sala 7:\nSegunda-Feira\n7h30-11h30',
+                                textAlign: TextAlign.start,
+                                style: TextStyle(
+                                  fontFamily: 'Readex Pro',
+                                  color: Color(0xFF650000),
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            )
+                            
+                          ],
+                        ),
                       ),
-                    ),
-                  )
-                ],
-              ),
-            ))
+                    )
+                  ],
+                ),
+              )
+            )
           ],
         ),
       ),
