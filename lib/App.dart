1b838d42f984dfe20da63adf684ad6074da08c86
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-09-02 13:09:38 -0300

    :goal_net: feat: Criando tela de erro e aplicando-a

--- lib/App.dart
+++ lib/App.dart
@@ -2,6 +2,7 @@ import 'package:agendamentos/views/pages/TelaLogin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './views/pages/NavBar.dart';
+import './views/pages/Erros/ErrorPage.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -22,13 +23,16 @@ class App extends StatelessWidget {
 
       initialRoute: '/',
       routes:{
-        '/':(context) => TelaLogin(),
+        '/': (context) => TelaLogin(),
         '/home': (context) {
           final args = ModalRoute.of(context)?.settings.arguments;
           if (args is bool) {
             return Navbar(isAdm: args);
           }
-          return TelaLogin(); //Erro
+          
+          return const ErrorPage(
+            erroMensagem: 'Desculpe, mas parece que houve um problema ao tentar exibir a página inicial. Por favor, tente novamente',
+          );
         },
       },
     );
---
+++ lib/views/pages/Erros/ErrorPage.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'dart:core'; 
+import 'package:url_launcher/url_launcher.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+
+class ErrorPage extends StatefulWidget {
+
+  final erroMensagem;
+  const ErrorPage({
+    super.key, 
+    required this.erroMensagem
+  });
+
+  @override
+  State<ErrorPage> createState() => _ErrorPageState();
+}
+
+class _ErrorPageState extends State<ErrorPage> {
+  final emailLaunchUri = Uri(
+    scheme: 'mailto',
+    path: 'kaua.moreno2005@gmail.com',
+    queryParameters: <String, String>{ 
+      'subject': 'Relatando erro no aplicativo',
+    },
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Erro'),
+      ),
+
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20,20,20,20),
+        child: Column(
+          children: [
+
+            const SizedBox(height: 40), //Espaço
+            const Text(
+              'Ops! Algo deu errado :/',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+
+            const SizedBox(height: 40), //Espaço
+            SingleChildScrollView(
+              child: AutoSizeText(
+                widget.erroMensagem,
+                textAlign: TextAlign.justify,
+                style: const TextStyle(
+                  fontSize: 20,
+                ),
+                minFontSize: 14,
+                maxLines: 7,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+
+            const Spacer(),
+
+            ElevatedButton(
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all<Size>(const Size(350, 60)), 
+              ),
+              child: const Text('Voltar'),
+              
+              onPressed: () => { 
+                Navigator.of(context).pushReplacementNamed('/')
+              },
+            ),
+
+            const SizedBox(height: 20), //Espaço
+
+            ElevatedButton(
+              style: ButtonStyle(
+                minimumSize: MaterialStateProperty.all<Size>(const Size(350, 60)),  
+              ),
+              child: const Text('Relatar Problema'),
+
+              onPressed: () => { 
+                launchUrl(emailLaunchUri)
+              },
+            ),
+                
+            const SizedBox(height: 100), //Espaço
+            
+          ],
+        ),
+      )
+    );
+  }
+}
