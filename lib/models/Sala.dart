8acf4332bbd102a3d2ded289a5856c437285e3fb
Aluno <kaua.moreno2005@gmail.com>
2023-09-05 10:21:30 -0300

    :recycle: refactor: Refatorando models/services relacionados a sala

--- lib/models/Sala.dart
+++ lib/models/Sala.dart
@@ -3,12 +3,10 @@ class Sala {
   final String nomeSala;
   final bool isLivre;
   final int capacidade;
-  final List<List<DateTime>> agendamentos; //Array de datas - incial e final 
 
   Sala({
     required this.isLivre, 
     required this.capacidade, 
     required this.nomeSala,
-    required this.agendamentos,
   });
 }
