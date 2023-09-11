a36d7ec3a8503278d171299ba87cddf573625861
Aluno <kaua.moreno2005@gmail.com>
2023-09-11 10:25:17 -0300

    :sparkles: feat: Redirecionando para outra página

---
+++ lib/views/pages/Agenda/Calendario.dart
@@ -0,0 +1,126 @@
+import 'package:agendamentos/views/components/CustomAppBar.dart';
+import 'package:agendamentos/views/constants/Texto.dart';
+import 'package:agendamentos/views/components/AgendaWidgets/botao.dart';
+import 'package:date_picker_timeline/date_picker_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+
+class Calendario extends StatefulWidget {
+  const Calendario({super.key});
+
+  @override
+  State<Calendario> createState() => _CalendarioState();
+}
+
+class _CalendarioState extends State<Calendario> {
+  DateTime _dataSelecionanda = DateTime.now();
+  late String notificacao;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(titulo: "Calendário"),
+      body: Column(
+        children: [
+          addAgendamento(),
+          addDataBar(),
+          const SizedBox(height: 10)
+        ]
+      ),
+    );
+  }
+
+  botoesCancelareCompletar({
+    required String label,
+    required Function()? onTap,
+    required Color clr,
+    required BuildContext context,
+    bool fechar = false
+  }) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 4),
+        height: 55,
+        width: MediaQuery.of(context).size.width * 0.9,
+        color: fechar == true ? Colors.red : clr,
+        decoration: BoxDecoration(
+          //PAREI AQUI 45:00
+        ),
+      ),
+    );
+  }
+
+  addDataBar() {
+    return Container(
+      margin: const EdgeInsets.only(top: 20, left: 20),
+      child: DatePicker(
+        //USADO PARA CHAMAR UMA LISTA COM VARIAS DATAS
+        DateTime.now(),
+        height: 100,
+        width: 80,
+        initialSelectedDate: DateTime.now(),
+        selectionColor:const Color.fromARGB(255, 78, 81, 247),
+        selectedTextColor: Colors.white,
+        dateTextStyle: GoogleFonts.lato(
+          textStyle: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          )
+        ),
+        dayTextStyle: GoogleFonts.lato(
+          textStyle: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          )
+        ),
+        monthTextStyle: GoogleFonts.lato(
+          textStyle: const TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w600,
+            color: Colors.grey,
+          )
+        ),
+        onDateChange: (data) {
+          _dataSelecionanda = data;
+        },
+      ),
+    );
+  }
+
+  addAgendamento() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  DateFormat.yMMMMd().format(DateTime.now()),
+                  style: subHeadingStyle,
+                ),
+                Text(
+                  "Hoje",
+                  style: headingStyle,
+                ),
+              ],
+            ),
+          ),
+          Botao(
+            label: "+Add Ag",
+            onTap: () {
+              Navigator.of(context).pushNamed('/agendamento');
+            }
+          )
+        ],
+      ),
+    );
+  }
+}
