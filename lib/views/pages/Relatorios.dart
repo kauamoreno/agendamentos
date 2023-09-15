d32e0508851845711411abcef2af575878f5d1f7
Kevin Taiyo <119669310+kevintaiyo@users.noreply.github.com>
2023-09-15 07:48:51 -0300

    :lipstick: feat: estilização da pagina de relatorio (#29)

--- lib/views/pages/Relatorios.dart
+++ lib/views/pages/Relatorios.dart
@@ -1,5 +1,3 @@
-import 'package:agendamentos/views/components/CustomAppBar.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,9 +8,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Relatórios',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: RelatoriosPage(),
     );
@@ -25,50 +24,83 @@ class RelatoriosPage extends StatefulWidget {
 }
 
 class _RelatoriosPageState extends State<RelatoriosPage> {
-  String selecionarTipoSala = 'Todas as Salas'; // Valor padrão
+  String selectedRoomType = 'Todas as Salas'; // Valor padrão
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: CustomAppBar(titulo: "Relatorios"),
+      appBar: AppBar(
+        title: Text('Relatórios'),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start, // Alinhar à esquerda
           children: <Widget>[
-            Icon(
-              Icons.analytics,
-              size: 100.0,
-              color: Colors.red,
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(
+                    Icons.analytics,
+                    color: Colors.red,
+                    size: 50.0,
+                  ),
+                ),
+                Text(
+                  'Filtrar por Sala:',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(16.5),
+                  child: DropdownButton<String>(
+                    icon: Icon(Icons.arrow_drop_down_sharp, color: Colors.black,),
+                    borderRadius: BorderRadius.circular(10),
+                    style: TextStyle(
+                      color: Colors.black,
+                      
+                    ),
+                    value: selectedRoomType,
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedRoomType = newValue!;
+                      });
+                    },
+                    items: <String>[
+                      'Todas as Salas',
+                      'Salas de Aula',
+                      'Salas de Informática',
+                      'Salas Eletroeletrônica',
+                      'Salas de Mecânica',
+                    ].map<DropdownMenuItem<String>>(
+                        (String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }
+                      ).toList(),
+                  ),
+                ),
+              ],
             ),
-            SizedBox(height: 20.0),
-            Text(
-              'Seus Relatórios',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
+            //FUNÇÕES DE VISUALIZAÇÃO DE RELATORIO NESTA PARTE
+            Container(
+              child: SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    children: [
+                      Text("Carlos Ferreira"),
+                      Text("Instrutor"),
+                      Text("Agendou Sala de informatica"),
+                      Text("17:00 - 18:00")
+                    ]
+                  ),
+                ),
               ),
-            ),
-            SizedBox(height: 20.0),
-            DropdownButton<String>(
-              value: selecionarTipoSala,
-              onChanged: (String? newValue) {
-                setState(() {
-                  selecionarTipoSala = newValue!;
-                });
-              },
-              items: <String>[
-                'Todas as Salas',
-                'Salas de Aula',
-                'Salas de Informática',
-                'Salas Eletroeletrônica',
-                'Salas de Mecânica',
-              ].map<DropdownMenuItem<String>>((String value) {
-                return DropdownMenuItem<String>(
-                  value: value,
-                  child: Text(value),
-                );
-              }).toList(),
-            ),
+            ), 
           ],
         ),
       ),
