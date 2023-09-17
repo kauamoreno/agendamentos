1915550cf65e40fe244f620a54074018ba9f3e7b
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-09-17 18:36:16 -0300

    :sparkles: feat: Criando sala, View e ViewModel, e configurando rota

--- lib/App.dart
+++ lib/App.dart
@@ -2,8 +2,10 @@ import 'package:agendamentos/views/pages/Erros/ErrorPage.dart';
 import 'package:agendamentos/views/pages/GerenciarUser.dart';
 import 'package:agendamentos/views/pages/Home/AdmHome.dart';
 import 'package:agendamentos/views/pages/Home/UserHome.dart';
+import 'package:agendamentos/views/pages/Sala/SalasConjunto.dart';
 import 'package:agendamentos/views/pages/TelaLogin.dart';
 import 'package:agendamentos/views/pages/Agenda/AddAgendamento.dart';
+import 'package:agendamentos/views/pages/TelaSalas.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './views/pages/Relatorios.dart';
@@ -26,15 +28,17 @@ class App extends StatelessWidget {
         primarySwatch: Colors.red
       ),
 
-      initialRoute: '/',
+      initialRoute: '/salasConjunto',
       routes:{
         '/': (context) => TelaLogin(),
-        '/userHome':(context) => UserHome(),
-        '/admHome':(context) => AdmHome(),
+        '/userHome':(context) => const UserHome(),
+        '/admHome':(context) => const AdmHome(),
         '/relatorio': (context) => RelatoriosPage(),
         '/gerenciarProfessores': (context) => const GerenciarProf(),
         '/calendario': (context) => const Calendario(),
         '/agendamento': (context) => const AddAgendamento(),
+        '/salasConjunto': (context) => const SalasConjunto(),
+        '/salas': (context) => const TelaSalas(),
         '/erro': (context) { 
           final args = ModalRoute.of(context)?.settings.arguments;
           if (args is bool) {
