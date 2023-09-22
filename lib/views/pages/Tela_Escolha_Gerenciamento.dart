fab1031bd65b90c0473a5c95f63d2c9f70e8ace8
Kevin Taiyo <119669310+kevintaiyo@users.noreply.github.com>
2023-09-22 08:05:33 -0300

    :ok_hand: style: alterando rotas no gerenciamento na homeAdm

---
+++ lib/views/pages/Tela_Escolha_Gerenciamento.dart
@@ -0,0 +1,37 @@
+import 'package:agendamentos/views/components/CustomAppBar.dart';
+import 'package:agendamentos/views/components/botoes_homes.dart';
+import 'package:flutter/material.dart';
+
+
+class TelaEscolhaGerenciamento extends StatefulWidget {
+  const TelaEscolhaGerenciamento({super.key});
+
+  @override
+  State<TelaEscolhaGerenciamento> createState() => _TelaEscolhaGerenciamentoState();
+}
+
+class _TelaEscolhaGerenciamentoState extends State<TelaEscolhaGerenciamento> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: CustomAppBar(titulo: ''),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 1, //
+                children: <Widget>[
+                  BotoesHome(context, 'Gerenciar Professores', Icons.settings, Colors.grey, '/gerenciarProfessores'),
+                  BotoesHome(context, 'Gerenciar Salas', Icons.settings, Colors.grey, '/gerenciarSalas'),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
