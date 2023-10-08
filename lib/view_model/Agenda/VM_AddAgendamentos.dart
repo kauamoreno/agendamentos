5b581f56c4e2a9cfd55769b55752edbdfd6c5f9c
Kauã Moreno <kaua.moreno2005@gmail.com>
2023-10-08 11:48:33 -0300

    :sparkles: feat: Verificando agendas repetidas e horarios invalidos

--- lib/view_model/Agenda/VM_AddAgendamentos.dart
+++ lib/view_model/Agenda/VM_AddAgendamentos.dart
@@ -1,5 +1,7 @@
 import 'package:agendamentos/view_model/SnackBarViewModel.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import '../../models/services/Firestore/SalasFirestore.dart';
 
 class VM_AddAgendamentos {
@@ -24,25 +26,76 @@ class VM_AddAgendamentos {
     required String timeFinal,
     required String lembrete,
     required String nomeProfessor,
-  }) {
+  }) async {
 
-    if(titulo.isNotEmpty /*Ver se não tem horarios iguais*/){
-      fire.criarAgendamento(
-        context: context, 
-        nomeConjunto: nomeConjunto, 
-        nomeSala: nomeSala,
-        titulo: titulo, 
-        data: data, 
-        nota: nota,
-        timeInicial: timeInicial, 
-        timeFinal: timeFinal, 
-        lembrete: lembrete,
-        nomeProfessor: nomeProfessor
-      );
-      Navigator.pop(context);
-    } else {
-      snack.erro(context, 'Por favor preencher todos os campos');
-    }
-    
+    if(titulo.isNotEmpty){
+      if(isHorarioValido(timeInicial, timeFinal)){
+        if(await semConflitoDeAgendamento(context, data, timeInicial, timeFinal)) {
+          fire.criarAgendamento(
+            context: context, 
+            nomeConjunto: nomeConjunto, 
+            nomeSala: nomeSala,
+            titulo: titulo, 
+            data: data, 
+            nota: nota,
+            timeInicial: timeInicial, 
+            timeFinal: timeFinal, 
+            lembrete: lembrete,
+            nomeProfessor: nomeProfessor
+          );
+          Navigator.pop(context);
+        } else { snack.erro(context, 'Horário ocupado, por favor escolher outro'); }
+      } else { snack.erro(context, 'Horário Inválido'); }
+    } else { snack.erro(context, 'Por favor preencher todos os campos'); }
   }
+
+  // Função para verificar se o horário inicial é menor que o horário final
+  bool isHorarioValido(String horaInicial, String horaFinal) {
+    DateTime horaInicialDateTime = DateFormat('HH:mm').parse(horaInicial);
+    DateTime horaFinalDateTime = DateFormat('HH:mm').parse(horaFinal);
+
+    return horaInicialDateTime.isBefore(horaFinalDateTime);
+  }
+
+  Future<bool> semConflitoDeAgendamento(BuildContext context, String dataSelecionada, String horaInicial, String horaFinal) async {
+    try {
+      DocumentReference salaConjuntoDocRef = FirebaseFirestore.instance.collection('salaConjunto').doc(nomeConjunto);
+      DocumentSnapshot salaConjuntoDoc = await salaConjuntoDocRef.get();
+
+      final salaConjuntoData = salaConjuntoDoc.data() as Map<String, dynamic>?;
+
+      final salas = salaConjuntoData?['Salas'] as List<dynamic>?;
+      final salaEspecifica = salas?.firstWhere((sala) => sala['nome'] == nomeSala, orElse: () => null);
+      final agendamentos = salaEspecifica?['agendamentos'] as List<dynamic>?;
+
+      if (agendamentos!.isNotEmpty) { // Esse agendamento tem que ser do dia que ele escolheu - ARRUMAR
+
+        // Converte as strings de hora inicial, final e data 
+        DateTime horaInicialDateTime = DateFormat('HH:mm').parse(horaInicial);
+        DateTime horaFinalDateTime = DateFormat('HH:mm').parse(horaFinal);
+        DateTime dataSelecionadaFormatada = DateFormat('dd/MM/yyyy').parse(dataSelecionada);
+
+        // Filtra os agendamentos para obter apenas aqueles na data selecionada
+        bool semHorariosConflitantes = agendamentos.any((agendamento) {
+          final dataAgendamento = DateFormat('dd/MM/yyyy').parse(agendamento['data']); 
+          final horaInicialAgendamento = DateFormat('HH:mm').parse(agendamento['timeInicial']);
+          final horaFinalAgendamento = DateFormat('HH:mm').parse(agendamento['timeFinal']);
+
+          // Verifica se há um conflito entre os horários
+          return (dataAgendamento == dataSelecionadaFormatada && 
+          (horaInicialAgendamento.isBefore(horaFinalDateTime) && horaFinalAgendamento.isAfter(horaInicialDateTime)));
+
+        });
+
+        return !semHorariosConflitantes;
+
+      } else {
+        return true;
+      }
+    } catch (error) {
+      snack.erro(context, 'Erro ao obter agendamentos: $error');
+      throw Exception('Erro ao obter agendamentos');
+    }
+  }
+
 }
