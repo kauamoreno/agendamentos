ed3f8f0ecd15408533a4acc38668abe9a61cce97
unknown <kevintaiyo17@gmail.com>
2023-10-08 20:58:02 -0300

    :lipstick: style: arrumando interface das homes e arrumando a escolha de gerenciamento

--- lib/views/pages/Home/UserHome.dart
+++ lib/views/pages/Home/UserHome.dart
@@ -13,18 +13,25 @@ class _UserHomeState extends State<UserHome> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const CustomAppBar(titulo: 'Home - Professor'),
-      body: Center(
-        child: GridView.count(
-          crossAxisCount: 2, // 2 colunas
+        appBar: const CustomAppBar(titulo: 'Home - Professor'),
+        body: Column(
           children: [
-            BotoesHome(context, 'Agenda', Icons.calendar_today,Colors.grey, '/calendario'),
-            BotoesHome(context, 'Feedback', Icons.feedback,Colors.grey, '/feedback'),
-            BotoesHome(context, 'Salas', Icons.room,Colors.grey, '/salasConjunto'),
-            BotoesHome(context, 'Notícias', Icons.new_releases,Colors.grey, '/noticias'),
+            Center(
+              child: GridView.count(
+                crossAxisCount: 2, // 2 colunas
+                children: [
+                  BotoesHome(context, 'Agenda', Icons.calendar_today,
+                      Colors.grey, '/calendario'),
+                  BotoesHome(context, 'Feedback', Icons.feedback, Colors.grey,
+                      '/feedback'),
+                  BotoesHome(context, 'Salas', Icons.room, Colors.grey,
+                      '/salasConjunto'),
+                  BotoesHome(context, 'Notícias', Icons.new_releases,
+                      Colors.grey, '/noticias'),
+                ],
+              ),
+            ),
           ],
-        ),
-      ),
-    );
+        ));
   }
 }
