6d501203107570811abda73c2a95bfd059ab8ec5
Kauã <kaua.moreno2005@gmail.com>
2023-10-28 12:27:06 -0300

    :sparkles: feat: Melhorando UX com login persistente

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -8,5 +9,9 @@ void main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Login persistente
+  await FirebaseAuth.instance.setPersistence(Persistence.LOCAL);
+
   runApp(const App());
 }
